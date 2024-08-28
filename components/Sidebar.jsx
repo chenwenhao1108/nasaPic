072591001444f918f3baf1ce360b2c9640926a62
chenwenhao1108 <wenwen69607@gmail.com>
2024-08-28 23:27:32 +0800

    first commit

---
+++ components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Sidebar(props) {
+    const { handleToggleModal, data } = props
+
+  return (
+    <div className='sidebar'>
+        <div className='bgOverlay' onClick={handleToggleModal}></div>
+        <div className='sidebarContents'>
+            <h2>{data?.title}</h2>
+            <div className='descriptionContainer'>
+                <p className='descriptionTitle'>{data?.date}</p>
+                <p>{data?.explanation}</p>
+            </div>
+            <button>
+                <i class="fa-solid fa-right-long"></i>
+            </button>
+        </div>
+    </div>
+  )
+}
